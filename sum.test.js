3adea615c5560c7782ccfe5df0ef0adb7c1ce563
BrianARuff <briananthonyruff@gmail.com>
2021-03-27 11:40:10 -0400

    iterables

--- sum.test.js
+++ sum.test.js
@@ -73,3 +73,17 @@ test("there is no I in team", () => {
 test('but there is a "stop" in Christoph', () => {
   expect("Christoph").toMatch(/stop/);
 });
+
+// arrays
+const shoppingList = [
+  "diapers",
+  "kleenex",
+  "trash bags",
+  "paper towels",
+  "beer",
+];
+
+test("the shopping list has beer on it", () => {
+  expect(shoppingList).toContain("beer");
+  expect(new Set(shoppingList)).toContain("beer");
+});

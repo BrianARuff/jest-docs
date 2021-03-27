848fd8d22ac6b35e2c8e4cc594464b9c27b3acde
BrianARuff <briananthonyruff@gmail.com>
2021-03-26 23:35:18 -0400

    common matchers => toBe, toEqual, not.toBe, not.toEqual

---
+++ sum.test.js
@@ -0,0 +1,23 @@
+const sum = require("./sum");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("2 plus 2 is 4", () => {
+  expect(2 + 2).toBe(4);
+});
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
